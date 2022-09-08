5d95f67dd19226cbb1396641b5348b9b5717f49e
Compotation <46853874+Compotation@users.noreply.github.com>
2022-09-08 11:44:33 -0400

    task c

---
+++ edit-array.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+int main() {
+	int myData[10];
+	for(int i = 0; i < 10; i++) {
+		myData[i] = 1;
+	}	
+
+	int i;
+	int v;
+	do {
+		std::cout << std::endl;
+		for(int i = 0; i < 10; i++) {
+			std::cout << myData[i] << " ";
+		}
+		std::cout << std::endl;
+
+		std::cout << std::endl << "Input index: ";
+		std::cin >> i;
+
+		std::cout << "Input value: ";
+		std::cin >> v;
+
+		if (i < 10) {
+			myData[i] = v;
+		} else {
+			std::cout << std::endl << "Index out of range. Exit." << std::endl;
+		}
+	} while (i < 10);
+	return 0;
+}
